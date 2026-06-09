d14c6c3a1b81f02c58cf0dc2f4bdcb8eca0f2a6e
likingaxis <luca.gugliotta567@gmail.com>
2026-06-09 20:06:55 +0200

    server.js configurato per funzionare con richieste http da rest API

--- server.js
+++ server.js
@@ -1,10 +1,80 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const examsFilePath = path.join(__dirname, "data", "exams.json");
 const app = express();
 const PORT = 3000;
 
 // AGGIUNTA MIDDLEWERE PER PARSING JSON E SERVIRE FILE STATICI
 app.use(express.json());
 app.use(express.static("public"));
+
+function readExams() {
+  const data = fs.readFileSync(examsFilePath, "utf-8");
+  return JSON.parse(data);
+}
+function writeExams(exams) {
+  fs.writeFileSync(examsFilePath, JSON.stringify(exams, null, 2));  //indentazione di 2 spazi per leggibilità
+}
+
+
+app.get("/api/exams", (req, res) => {
+  const exams = readExams();
+  res.json(exams);
+});
+
+app.get("/api/exams/:id", (req, res) => {
+  const exams = readExams();
+  const id = Number(req.params.id); //id arriva come stringa quindi ho messo un casting a numero
+
+  const exam = exams.find((item) => item.id === id);
+
+  if (!exam) {
+    return res.status(404).json({
+      error: "Appello non trovato"
+    });
+  }
+
+  res.json(exam);
+});
+
+app.post("/api/exams", (req, res) => {
+  const exams = readExams();
+
+  const {
+    course,
+    teacher,
+    date,
+    classroom,
+    type,
+    description
+  } = req.body;
+
+  if (!course || !teacher || !date || !classroom || !type) {
+    return res.status(400).json({      //errore 400 Bad Request
+      error: "Compila tutti i campi obbligatori"
+    });
+  }
+
+  const newExam = {
+    id: Date.now(),
+    course,
+    teacher,
+    date,
+    classroom,
+    type,
+    description: description || ""
+  };
+
+  exams.push(newExam);
+  writeExams(exams);
+
+  res.status(201).json({    //errore 201 serve per indicare che la risorsa è stata creata con successo
+    message: "Appello creato con successo",
+    exam: newExam
+  });
+});
+
 
 app.get("/api/status", (req, res) => {
   res.json({
