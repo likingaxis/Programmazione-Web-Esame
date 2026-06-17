cfd24ccc61956d737e547a95b5f7a6595b8b8ddf
likingaxis <luca.gugliotta567@gmail.com>
2026-06-17 17:13:25 +0200

    aggiunto errore 500 e 404, migliorata indentazione, migliorato README, e errori vari

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # Programmazione-Web-Esame
 Esame di Programmazione Web progetto
 
+
+
+# Portale Informatica Tor Vergata
+
+## Descrizione
+
+## Installazione
+
 npm init -y
 npm install express
-npm install nodemon
+npm install --save-dev nodemon
+
+## Avvio
+
 npm run dev
+
+## Funzionalità implementate
+
+## API disponibili
+
+## Funzionalità extra
+
+## Uso dell'AI
--- server.js
+++ server.js
@@ -11,95 +11,137 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.static("public"));
 
+
+// esami
 function readExams() {
   const data = fs.readFileSync(examsFilePath, "utf-8");
   return JSON.parse(data);
 }
+
 function writeExams(exams) {
   fs.writeFileSync(examsFilePath, JSON.stringify(exams, null, 2));  //indentazione di 2 spazi per leggibilità
 }
 
+
+// lezioni
 function readLessons(){
-  const data= fs.readFileSync(lessonsFilePath, "utf-8");
-  return JSON.parse(data);
+    const data= fs.readFileSync(lessonsFilePath, "utf-8");
+    return JSON.parse(data);
 }
 
+// comunicazioni
 function readCommunications(){
-  const data= fs.readFileSync(communicationsFilePath, "utf-8");
-  return JSON.parse(data);
+    const data= fs.readFileSync(communicationsFilePath, "utf-8");
+    return JSON.parse(data);
 }
 
-app.get("/api/lessons", (req, res) => {
-  const lessons = readLessons();
-  res.json(lessons);
-});
-
-app.get("/api/communications", (req, res) => {
-  const communications = readCommunications();
-  res.json(communications);
-});
-
-app.get("/api/exams", (req, res) => {
-  const exams = readExams();
-  res.json(exams);
-});
-
-app.get("/api/exams/:id", (req, res) => {
-  const exams = readExams();
-  const id = Number(req.params.id); //id arriva come stringa quindi ho messo un casting a numero
-
-  const exam = exams.find((item) => item.id === id);
-
-  if (!exam) {
-    return res.status(404).json({
-      error: "Appello non trovato"
-    });
+// endpoint api
+app.get("/api/lessons", (req, res, next) => {
+  try{
+    const lessons = readLessons();
+    res.json(lessons);
+  }catch (error) {
+    next(error);
   }
-
-  res.json(exam);
 });
 
-app.post("/api/exams", (req, res) => {
-  const exams = readExams();
-
-  const {
-    course,
-    teacher,
-    date,
-    classroom,
-    type,
-    description
-  } = req.body;
-
-  if (!course || !teacher || !date || !classroom || !type) {
-    return res.status(400).json({      //errore 400 Bad Request
-      error: "Compila tutti i campi obbligatori"
-    });
+app.get("/api/communications", (req, res, next) => {
+  try{
+    const communications = readCommunications();
+    res.json(communications);
+  }catch (error) {
+    next(error);
   }
-
-  const newExam = {
-    id: Date.now(),
-    course,
-    teacher,
-    date,
-    classroom,
-    type,
-    description: description || ""
-  };
-
-  exams.push(newExam);
-  writeExams(exams);
-
-  res.status(201).json({    //errore 201 serve per indicare che la risorsa è stata creata con successo
-    message: "Appello creato con successo",
-    exam: newExam
-  });
 });
 
+app.get("/api/exams", (req, res, next) => {
+  try{
+    const exams = readExams();
+    res.json(exams);
+  }catch (error) {
+    next(error);
+  }
+});
 
+// ottenere un singolo esame in base a un id 
+app.get("/api/exams/:id", (req, res, next) => {
+  try{
+    const exams = readExams();
+    const id = Number(req.params.id); //id arriva come stringa quindi ho messo un casting a numero
+
+    const exam = exams.find((item) => item.id === id);
+
+    if (!exam) {
+      return res.status(404).json({
+        error: "Appello non trovato"
+      });
+  }
+    res.json(exam);
+  }catch (error) {
+    next(error);
+  }
+});
+
+// endpoint per aggiunta di un esame
+app.post("/api/exams", (req, res, next) => {
+  try{
+    const exams = readExams();
+
+    const {
+      course,
+      teacher,
+      date,
+      classroom,
+      type,
+      description
+    } = req.body;
+
+    if (!course || !teacher || !date || !classroom || !type) {
+      return res.status(400).json({      //errore 400 Bad Request
+        error: "Compila tutti i campi obbligatori"
+      });
+    }
+
+    const newExam = {
+      id: Date.now(),
+      course,
+      teacher,
+      date,
+      classroom,
+      type,
+      description: description || ""
+    };
+
+    exams.push(newExam);
+    writeExams(exams);
+
+    res.status(201).json({    //messaggio di successo 201 serve per indicare che la risorsa è stata creata con successo
+      message: "Appello creato con successo",
+      exam: newExam
+    });
+  }catch (error) {
+    next(error);
+  }
+});
+
+// verificare lo stato del server
 app.get("/api/status", (req, res) => {
   res.json({
     message: "Server Express funzionante"
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Rotta non trovata"
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+    error: "Errore interno del server"
   });
 });
 
